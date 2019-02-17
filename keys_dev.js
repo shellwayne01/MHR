125c98a7dd0ed1dc3dff454c4a8a3da789fe563b
Yorranshell Wattson <shellwayne01@users.noreply.github.com>
2019-02-17 16:42:20 -0500

    Delete keys_dev.js

--- keys_dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://Admin-Shelly:honeybunches@firstcluster-shard-00-00-ak45t.mongodb.net:27017,firstcluster-shard-00-01-ak45t.mongodb.net:27017,firstcluster-shard-00-02-ak45t.mongodb.net:27017/test?ssl=true&replicaSet=FirstCluster-shard-0&authSource=admin&retryWrites=true',
-  secretOrKey: 'sike'
-};
