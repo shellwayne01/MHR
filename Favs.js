bbd7e39d9cc011c5fb3eba68fa9850ddd8887265
shellwayne01 <shellwayne01@gmail.com>
2019-06-04 13:15:52 -0400

    updated

--- Favs.js
+++ Favs.js
@@ -1,1 +1,8 @@
-//Modify later
+var map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 13
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',
+{foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+}).addTo(map);
