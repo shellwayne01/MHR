2bc48af9757b609ef40d9cb52a55add681e979df
shellwayne01 <shellwayne01@gmail.com>
2019-06-11 11:09:25 -0400

    updated with private keys

---
+++ keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://Admin-Shelly:mangojuice@firstcluster-shard-00-00-ak45t.mongodb.net:27017,firstcluster-shard-00-01-ak45t.mongodb.net:27017,firstcluster-shard-00-02-ak45t.mongodb.net:27017/test?ssl=true&replicaSet=FirstCluster-shard-0&authSource=admin&retryWrites=true',
+  secretOrKey: 'sike'
+};
