9bcf6825a6ce514a9048f749d1a55a80733ea526
shellwayne01 <shellwayne01@gmail.com>
2019-06-19 18:26:57 -0400

    Updated

--- keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // mongodb://[username:password@]host1[:port1][,...hostN[:portN]]][/[database][?options]]
-  // mongoURI: 'mongodb://Admin-Shelly:mangojuice@firstcluster-shard-00-00-ak45t.mongodb.net:27017,firstcluster-shard-00-01-ak45t.mongodb.net:27017,firstcluster-shard-00-02-ak45t.mongodb.net:27017/test?ssl=true&replicaSet=FirstCluster-shard-0&authSource=admin&retryWrites=true',
-  mongoURI: 'mongodb+srv://Admin-Shelly:mangojuice@firstcluster-ak45t.mongodb.net/MHR-DB?retryWrites=true&w=majority',
-  secretOrKey: 'sike'
-};
