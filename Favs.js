2ee06854d136c2438647678d0b98d6f271de797c
shellwayne01 <shellwayne01@gmail.com>
2019-08-24 12:28:54 -0400

    modified

--- Favs.js
+++ Favs.js
@@ -1,6 +1,6 @@
 var destinations = document.querySelectorAll("#intDest li")
-var lats  = [9.748917,18.044230,18.220833,9.081999,51.919437]
-var longs = [-83.753426,-64.835892,-66.590149,8.675277,19.145136]
+var lats  = [39.90,18.04,18.22,9.08,51.91] //north-south
+var longs = [116.40,-64.83,-66.59,8.67,19.14] //east-west
 
 // destinations.filter(check)
 // console.log(typeof destinations)
@@ -16,11 +16,14 @@ var map = L.map('map', {
 // var osmAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 // L.tileLayer(osmUrl,{foo: 'bar', attribution: osmAttr}).addTo(map);
 
-//I assume the issue with formatting is due to the tile server.
+
+// var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 var osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-var osmAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+// var osmAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+var osmAttr = 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>)'
 L.tileLayer(osmUrl,{attribution: osmAttr}).addTo(map);
 
+//Works just fine
 // var dest = destinations[0].innerHTML;
 // var marker = L.marker([lats[0], longs[0]]);
 // marker.bindPopup(dest).openPopup();
@@ -30,7 +33,12 @@ L.tileLayer(osmUrl,{attribution: osmAttr}).addTo(map);
 for (i=0; i<lats.length; i++){
   var dest = destinations[i].innerHTML;
   var marker = L.marker([lats[i], longs[i]]);
-  marker.bindPopup(dest).openPopup();
+
+  //Fix later
+  // var popup = L.popup()
+  //   .setContent(dest);
+  // marker.bindPopup(popup).openPopup();
+  // marker.bindPopup(dest).openPopup();
   marker.addTo(map);
 }
 
