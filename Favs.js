255891c921b508645f2bd9c7680d557841b355c0
shellwayne01 <shellwayne01@gmail.com>
2019-08-25 20:25:49 -0400

    Updated

--- Favs.js
+++ Favs.js
@@ -18,6 +18,15 @@ addListItems(valuesArray[2], keysArray[2]);
 addListItems(valuesArray[3], keysArray[3]);
 addListItems(valuesArray[4], keysArray[4]);
 
+//Carousels
+var galleryCollection = document.getElementsByClassName('gallery')
+console.log(galleryCollection)
+
+for(i=0; i< galleryCollection.length; i++){
+  console.log(galleryCollection[i].clientTop)
+  console.log(galleryCollection[i].clientWidth)
+  galleryCollection[i].addEventListener("mouseover", playGallery)
+}
 
 //Fix callback if possible to simplify population
 // for (i=0; i<keysArray.length;){
@@ -59,9 +68,94 @@ for (i=0; i<lats.length; i++){
   marker.addTo(map);
 }
 
+// Methods
 function addListItems(dataArray, containerId){
    var container = document.getElementById(containerId)
    for(i=0; i<dataArray.length; i++){
      container.innerHTML += '<li>'+dataArray[i]+'</li>'
    }
+}
+
+//FIX LATER
+// var id = setInterval(playGallery, 5); //wait to allow playGallery
+// function playGallery(){
+//   var images = this.children
+//   Transform images within gallery
+//   if ( ) {
+//     // clearInterval(id);
+//   }
+//   else {
+//     for (i=0; i<images.length; i++){
+//       images[i].style.animation = "moveLeft 15s infinite";
+//     }
+//   }
+
+
+  //Transform Entire Gallery
+  // this.style.transform = 'translateX(-300px)';
+
+  // console.log(this.style)
+  // if (test for finished ) {
+  //   clearInterval(id);
+  // } else {
+  //   code to change the element style
+  // }
+// }
+
+//Galleries
+var restArr = ["https://s3-media3.fl.yelpcdn.com/bphoto/5179Qe1e_rq0kOsi2aHELQ/ls.jpg",
+"https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i6JDE_NnDhiA/v1/-1x-1.jpg",
+"https://havanacafe.info/images/img_6194-small.jpg?crc=468475893",
+"https://www.simplyrecipes.com/wp-content/uploads/2019/04/Nachos-LEAD-3.jpg"]
+var restIndex = 0
+
+var movieArr = ["https://is1-ssl.mzstatic.com/image/thumb/Video118/v4/00/ee/3a/00ee3a87-eeda-6d4b-bb4b-5a45ef1e4579/contsched.vhzbldts.lsr/268x0w.jpg",
+"https://upload.wikimedia.org/wikipedia/en/thumb/3/3e/A_Bronx_Tale.jpg/220px-A_Bronx_Tale.jpg",
+"https://m.media-amazon.com/images/M/MV5BZmNjZWI3NzktYWI1Mi00OTAyLWJkNTYtMzUwYTFlZDA0Y2UwXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+"https://m.media-amazon.com/images/M/MV5BMGU5OWEwZDItNmNkMC00NzZmLTk1YTctNzVhZTJjM2NlZTVmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg",
+"https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"]
+var movieIndex = 0
+
+var bookArr = ["https://upload.wikimedia.org/wikipedia/en/thumb/e/e6/TreeGrowsInBrooklyn.jpg/220px-TreeGrowsInBrooklyn.jpg",
+"https://images-na.ssl-images-amazon.com/images/I/51NVHJqbUmL._SX329_BO1,204,203,200_.jpg",
+"https://images-na.ssl-images-amazon.com/images/I/41iffjvqdQL._SX322_BO1,204,203,200_.jpg",
+"https://pictures.abebooks.com/1LACEYLANE/3700700586.jpg"]
+var bookIndex = 0
+
+var Images = {"restaurantGallery2":[restArr, restIndex], "movieGallery2":[movieArr, movieIndex],"bookGallery2":[bookArr,bookIndex] }
+
+// Circular array not necessary
+// var counter = index % imageArr.length
+function prevImg(gallery){
+  console.log(gallery.id)
+  var query = '#'+ gallery.id +' .mainGalleryImg'
+  var mainImg = document.querySelector(query)
+  var imageArr = Images[gallery.id][0]
+  var index = Images[gallery.id][1]
+
+  if(index==0){
+    Images[gallery.id][1] = imageArr.length - 1
+  }else{
+    Images[gallery.id][1] -= 1
+  }
+  // console.log(Images[gallery.id][1])
+  // console.log(imageArr[index])
+  mainImg.src = imageArr[Images[gallery.id][1]]
+}
+
+function nextImg(gallery){
+  console.log(gallery.id)
+  var query = '#'+ gallery.id +' .mainGalleryImg'
+  var mainImg = document.querySelector(query)
+  var imageArr = Images[gallery.id][0]
+  var index = Images[gallery.id][1]
+
+  if(index == imageArr.length - 1){
+    Images[gallery.id][1] = 0
+  }else{
+    Images[gallery.id][1] += 1
+  }
+  // console.log(Images[gallery.id][1])
+  // console.log(imageArr[index])
+  mainImg.src = imageArr[Images[gallery.id][1]]
 }
